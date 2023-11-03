4bb6b5a9ab26f89f9f88c8d21198cd268ba4746a
Kevin <kevinponting82@gmail.com>
2023-11-03 01:13:22 -0700

    added comments

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+//select html elements
 const gameContainer = document.getElementById('game__container');
 const chessBoard = document.getElementById('chess__board');
 const resetButton = document.getElementById('reset__button');
 const gameStatus = document.getElementById('game__status');
 
+//state of the game at start
 const initialBoardState = [
   "b__rook", "b__knight", "b__bishop", "b__queen", "b__king", "b__bishop", "b__knight", "b__rook",
   "b__pawn", "b__pawn", "b__pawn", "b__pawn", "b__pawn", "b__pawn", "b__pawn", "b__pawn",
@@ -14,12 +16,12 @@ const initialBoardState = [
   "w__rook", "w__knight", "w__bishop", "w__queen", "w__king", "w__bishop", "w__knight", "w__rook"
 ];
 
+//initialize the board, current selected piece, and who's turn it is
 let board = [];
 let selectedPiece;
 let playerTurn;
 
-// Creates <div class= chess__square>  elements
-
+//create <div class=chess__square>  elements
 function createBoard(loadBoard) {
   board = [...loadBoard];
   console.log('board: ', board);
@@ -29,12 +31,12 @@ function createBoard(loadBoard) {
       square.setAttribute('square-id', rank * 8 + file);
       square.classList.add('chess__square');
 
-      // Alternating square color
+      //alternate square color
       if ((rank + file) % 2 === 0) {
         square.classList.add('dark');
       }
 
-      // Adding events to each square
+      //adding events
       square.addEventListener('dragover', dragOver);
       square.addEventListener('drop', dragDrop);
       chessBoard.appendChild(square);
@@ -43,8 +45,7 @@ function createBoard(loadBoard) {
 }
 
 
-// Add pieces to chess__square from variable: board
-
+//add pieces to chess__square from array: board
 function initializePieces() {
   let squares = document.querySelectorAll('.chess__square')
   for (let rank = 0; rank < 8; rank++) {
@@ -53,6 +54,7 @@ function initializePieces() {
       let pieceName = board[index];
 
       console.log('pieceName in intiliaze: ', pieceName);
+      //inject piece into square if not an empty space
       if (pieceName !== '') {
         let color = pieceName[0];
         let pieceImg = document.createElement('img');
@@ -70,19 +72,21 @@ function initializePieces() {
   let pieces = document.querySelectorAll('.chess__piece');
   console.log(pieces);
 
+  //add event listeners to each piece
   pieces.forEach(piece => {
     piece.addEventListener('dragstart', dragStart);
     piece.addEventListener('dragend', dragEnd);
   });
 }
 
-// Whenever a piece is dragged selected piece is set to it
+//whenever a piece is dragged, selected piece is set to it
 function dragStart(event) {
   selectedPiece = event.target;
   if (!selectedPiece.classList.contains(playerTurn)) { return; }
   let startPos = selectedPiece.parentNode.getAttribute('square-id');
 
   for (let i = 0; i < 64; i++) {
+    //check if legal move, if legal, highlight the legal squares
     if (isLegalMove(selectedPiece.alt, startPos, i)) {
       let square = document.querySelector('[square-id="' + i + '"]');
       square.classList.add("move__legal");
@@ -90,30 +94,33 @@ function dragStart(event) {
   }
 }
 
+//after dragging is stopped
 function dragEnd(event) {
   let squares = document.querySelectorAll('.move__legal');
   squares.forEach(square => {
+    //take off legal move highlights
     square.classList.remove('move__legal')
   });
 
 }
 
-// When the piece is dropped onto a new square
+//when piece is dropped onto a square
 function dragDrop(event) {
   event.stopPropagation();
   console.log(event.target);
   let square = event.target;
 
-  // Checks if moved piece color matches current player turn color
+  //check if moved piece color matches current player turn color
   if (!selectedPiece.classList.contains(playerTurn)) { return; }
-  // Finding the opponent color of current player
+  //find the opponent color of current player
   const opponent = playerTurn === 'w' ? 'b' : 'w';
-  // Checks if the square dropped onto contains a piece
+  //checks if the square dropped onto contains a piece
   const capture = square.classList.contains('chess__piece')
-  // Checks if the square dropped onto contains a piece from the opponent
+  //checks if the square dropped onto contains a piece from the opponent
   const piece = selectedPiece.alt;
   const startPos = selectedPiece.parentNode.getAttribute('square-id');
 
+  //if a capture is taking place
   let child = '';
   if (capture) {
     child = square;
@@ -121,14 +128,17 @@ function dragDrop(event) {
   }
 
   const endPos = square.getAttribute('square-id');
+
+  //check if this is a legal move
   console.log('is legal move: ', !isLegalMove(piece, startPos, endPos));
   if (!isLegalMove(piece, startPos, endPos)) { return; }
 
   console.log("the child: ", child);
   if (child !== '') {
     child.remove();
-    //check if the piece being taken is a king piece
+    //check if the piece being taken is a king
     if (child.alt === 'b__king' || child.alt === 'w__king') {
+      //game over :(
       if (child.alt === 'b__king') {
         alert('White has won!');
       }
@@ -136,6 +146,7 @@ function dragDrop(event) {
         alert('Black has won!');
       }
       
+      //reset the game after game over
       reset();
       createBoard(initialBoardState);
       initializePieces();
@@ -143,36 +154,39 @@ function dragDrop(event) {
     }
   }
 
+  //check for promotion
   if (piece === 'w__pawn' || piece === 'b__pawn') {
-    //check if its a pawn that can be promoted
     console.log('isPromotion(): ', isPromotion(piece, startPos, endPos));
     if (isPromotion(piece, startPos, endPos)) {
       Promote(startPos);
     }
   }
 
+  //move the piece to its new home
   square.appendChild(selectedPiece);
   board[endPos] = board[startPos];
   board[startPos] = '';
 
+  //check if castling
   if (piece === 'w__king' || piece === 'b__king') {
-    //check if castling
     console.log('isCastling: ', isCastling(piece, startPos, endPos));
     if (isCastling(piece, startPos, endPos)) {
-      //move rook
-      castle(piece, startPos, endPos);
+      castle(piece, startPos, endPos); //move rook
     }
   }
 
+  //change player turn
   changePlayer();
+  //save the current game state after move to keep game save up to date
   saveGame();
 }
 
+//dragging over a square
 function dragOver(event) {
   event.preventDefault();
 }
 
-// Switches player turn status and updates game status
+//switches player turn
 function changePlayer() {
   if (playerTurn === 'w') {
     playerTurn = 'b';
@@ -183,45 +197,45 @@ function changePlayer() {
   }
 }
 
-// CHESS LOGIC
+//CHESS LOGIC
 function isLegalMove(piece, startPos, endPos) {
   console.log(piece, startPos, endPos);
   let legal = false;
 
   startPos = Number(startPos);
   endPos = Number(endPos);
+
+  //CHECK IF PATH CLEAR FOR PAWN, QUEEN, KING, ROOK, AND BISHOP
   switch (piece) {
-    case 'w__pawn':
+    case 'w__pawn': //white pawn
       const w_startRank = [48, 49, 50, 51, 52, 53, 54, 55];
       //move two spaces if in starting position
       if (w_startRank.includes(startPos) && startPos + -8 * 2 === endPos) {
-        console.log('1');
         legal = isPathClear(startPos, endPos) && board[endPos] === '';
       }//move one space
       else if (startPos - 8 === endPos) {
-        console.log('2');
         legal = isPathClear(startPos, endPos) && board[endPos] === '';
       }//diagonal capture
       else if ((startPos - 7 === endPos || startPos - 9 === endPos) && board[endPos] !== '') {
-        console.log('3');
-
         legal = isPathClear(startPos, endPos) && (board[endPos] === '' ? true : board[endPos][0] !== playerTurn);
       }
       break;
 
-    case 'b__pawn':
+    case 'b__pawn': //black pawn
       const b_startRank = [8, 9, 10, 11, 12, 13, 14, 15];
+      //move two spaces if in starting position
       if (b_startRank.includes(startPos) && startPos + 8 * 2 === endPos) {
         legal = isPathClear(startPos, endPos) && board[endPos] === '';
-
-      } else if (startPos + 8 === endPos) {
+      }//move one space
+      else if (startPos + 8 === endPos) {
         legal = isPathClear(startPos, endPos) && board[endPos] === '';
-      } else if ((startPos + 7 === endPos || startPos + 9 === endPos) && board[endPos] !== '') {
+      }//diagonal capture
+      else if ((startPos + 7 === endPos || startPos + 9 === endPos) && board[endPos] !== '') {
         legal = isPathClear(startPos, endPos) && (board[endPos] === '' ? true : board[endPos][0] !== playerTurn);
       }
       break;
 
-    case 'w__knight':
+    case 'w__knight': //knights
     case 'b__knight':
       const knightOffset = [15, 17, 10, 6];
       if (knightOffset.includes(Math.abs(startPos - endPos))) {
@@ -239,20 +253,20 @@ function isLegalMove(piece, startPos, endPos) {
       }
       break;
 
-    case 'w__bishop':
+    case 'w__bishop': //bishops
     case 'b__bishop':
       if (Math.abs(startPos - endPos) % 9 === 0 || Math.abs(startPos - endPos) % 7 === 0) {
         legal = isPathClear(startPos, endPos) && (board[endPos] === '' ? true : board[endPos][0] !== playerTurn);
       }
       break;
 
-    case 'w__rook':
+    case 'w__rook': //rooks
     case 'b__rook':
       if (Math.abs(startPos - endPos) % 8 === 0 || Math.floor(startPos / 8) === Math.floor(endPos / 8)) {
         legal = isPathClear(startPos, endPos) && (board[endPos] === '' ? true : board[endPos][0] !== playerTurn);
       }
       break;
-    case 'w__queen':
+    case 'w__queen': //queens
     case 'b__queen':
       if (Math.abs(startPos - endPos) % 9 === 0 || Math.abs(startPos - endPos) % 7 === 0 ||
         Math.abs(startPos - endPos) % 8 === 0 || Math.abs(startPos - endPos) < startPos + 8
@@ -275,6 +289,8 @@ function isLegalMove(piece, startPos, endPos) {
       }
       break;
   }
+
+  //return legal if the path is clear and it moves according to the piece's limits
   console.log(legal);
   return legal;
 }
@@ -295,12 +311,17 @@ function isPromotion(piece, startPos, endPos) {
   }
 }
 
+//WOO PROMOTION
 function Promote(startPos) {
   console.log('promotion piece', selectedPiece);
   console.log(selectedPiece.src);
+
+  //promotion choices
   choices = ['Queen', 'Rook', 'Knight', 'Bishop'];
   let userChoice = '';
   let validChoice = true;
+
+  //until user enters a valid option, loop (case insensitive)
   while (validChoice) {
     userChoice = prompt(`Promotion choices: ${choices.join(', ')}`, 'Queen');
     if (choices.includes(userChoice.charAt(0).toUpperCase() + userChoice.slice(1))) {
@@ -310,17 +331,23 @@ function Promote(startPos) {
   console.log('alt:', selectedPiece.alt);
   console.log('alt:', selectedPiece.alt[0]);
 
+  //convert first letter of choice to lower case
   const lowerCase = userChoice.charAt(0).toLowerCase() + userChoice.slice(1);
   console.log('lowCase: ', lowerCase)
+
+  //set the source for the <img> to the new piece svg
   selectedPiece.src = `assets/${selectedPiece.alt[0]}` + '__' + `${lowerCase}.svg`;
   console.log('selected piece src: ', selectedPiece.src);
-  selectedPiece.alt = `${selectedPiece.alt[0]}__${lowerCase}`;
 
+  //set the alt for the <img> to the new piece name
+  selectedPiece.alt = `${selectedPiece.alt[0]}__${lowerCase}`;
   console.log('alt after promote:', selectedPiece.alt);
 
+  //update the name on the board array
   board[startPos] = `${selectedPiece.alt[0]}` + '__' + `${lowerCase}`;
 }
 
+//check if king is castling
 function isCastling(piece, startPos, endPos) {
   if (piece === 'w__king' && startPos == 60 && ((endPos == 62 && 'w__rook' === board[63]) || (endPos == 58 && 'w__rook' === board[56]))) {
     return true;
@@ -332,8 +359,11 @@ function isCastling(piece, startPos, endPos) {
     else return false;
 }
 
+//PROTECT THE KING
 function castle(piece, startPos, endPos) {
+  //could've been implemented better (DRY)
   if (piece === 'w__king') {
+    //castle w/ rook closest to king
     if (startPos == 60 && endPos == 62 && 'w__rook' === board[63]) {
       let rookSquare = document.querySelector('[square-id="63"]');
       let rook = rookSquare.querySelector('img');
@@ -350,6 +380,7 @@ function castle(piece, startPos, endPos) {
       console.log('im emptysquare', emptySquare);
     }
     else {
+      //castle w/ rook farthest from king
       let rookSquare = document.querySelector('[square-id="56"]');
       let rook = rookSquare.querySelector('img');
       let emptySquare = document.querySelector('[square-id="59"]');
@@ -366,6 +397,7 @@ function castle(piece, startPos, endPos) {
     }
   }
   if (piece === 'b__king') {
+    //castle w/ rook closest to king
     if (startPos == 4 && endPos == 6 && 'b__rook' === board[7]) {
       let rookSquare = document.querySelector('[square-id="7"]');
       let rook = rookSquare.querySelector('img');
@@ -382,6 +414,7 @@ function castle(piece, startPos, endPos) {
       console.log('im emptysquare', emptySquare);
     }
     else {
+      //castle w/ rook farthest from king
       let rookSquare = document.querySelector('[square-id="0"]');
       let rook = rookSquare.querySelector('img');
       let emptySquare = document.querySelector('[square-id="3"]');
@@ -400,6 +433,7 @@ function castle(piece, startPos, endPos) {
   console.log('board in castle: ', board);
 }
 
+//check if path is clear
 function isPathClear(startPos, endPos) {
   let startRank = Math.floor(startPos / 8);
   let startFile = startPos % 8;
@@ -422,16 +456,19 @@ function isPathClear(startPos, endPos) {
   return true;
 }
 
+//might not get to in time
 function isKingSafe() {
   // NEED TO IMPLEMENT
 }
 
+//reset game button
 resetButton.addEventListener('click', () => {
   reset();
   createBoard(initialBoardState);
   initializePieces();
 });
 
+//reset the game
 function reset() {
   localStorage.clear();
   selectedPiece = '';
@@ -443,6 +480,7 @@ function reset() {
   }
 }
 
+//save to local storage
 function saveGame() {
   console.log('initial board before saving: ', initialBoardState);
   console.log('board before saving: ', board);
@@ -450,6 +488,7 @@ function saveGame() {
   localStorage.setItem('savedPlayerTurn', playerTurn);
 }
 
+//load saved game from local storage
 function loadGame() {
   let loadedGame = localStorage.getItem('savedGame');
 
@@ -461,6 +500,7 @@ function loadGame() {
   return loadedGameArray;
 }
 
+//load player turn from local storage
 function loadPlayerTurn() {
   const loadedPlayerTurn = localStorage.getItem('savedPlayerTurn');
 
@@ -469,6 +509,7 @@ function loadPlayerTurn() {
   return loadedPlayerTurn;
 }
 
+//check if there was a game saved in local storage
 function checkForSavedGame() {
   //get saved game if present
   const gameSave =  localStorage.getItem('savedGame');
@@ -483,6 +524,7 @@ function checkForSavedGame() {
 
 // DRIVER START
 
+//if theres a saved game, load it
  if (checkForSavedGame()) {
    const loadedGame = loadGame();
    const loadedPlayerTurn = loadPlayerTurn();
@@ -497,13 +539,9 @@ function checkForSavedGame() {
    createBoard(loadedGame);
    initializePieces();
 } 
- else {
+ else { //if there isn't a saved game, load as if its a new game
    createBoard(initialBoardState);
    initializePieces();
    playerTurn = 'w';
    gameStatus.textContent = "White's turn.";
 }
-
-// Temporary will change to load with game save
-
-
